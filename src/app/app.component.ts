f3b90ac889c9c71a6588308653935fb43be1c3a0
Dexter Hardy <dhardy@HQPro.local>
2019-11-10 21:14:21 -0500

    update formating using prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,59 +3,54 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { AuthService } from 'projects/ngx-amplify/src/public-api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  
-  title = 'ngx-amplify-starter';
-  loginForm: FormGroup;
+    title = 'ngx-amplify-starter';
+    loginForm: FormGroup;
 
-  constructor(
-    private auth: AuthService,
-    private fb: FormBuilder,
-  ) {
-    this.buildForm();
-  }
-
-  buildForm() {
-    let self = this;
-
-    self.loginForm = this.fb.group(
-      {
-        username: [
-          '',
-          Validators.compose([
-            Validators.required,
-            Validators.minLength(5),
-            Validators.maxLength(25),
-          ]),
-        ],
-        password: ['', Validators.compose([Validators.required])],
-      },
-      { updateOn: 'blur' },
-    );
-  }
-
-  onEvent(event: string) {
-    let self = this;
-
-    if (event === 'login') {
-      let result = self.auth.signIn(self.loginForm.value).then(
-        (result) => {
-          console.log('authUser', result);
-        }
-      ).catch(
-        (err) => {
-          console.error('signIn', err);
-        }
-      )
+    constructor(private auth: AuthService, private fb: FormBuilder) {
+        this.buildForm();
     }
 
-    if (event === 'signOut') {
-      console.log('signOut', event);
-      self.auth.signOut();
+    buildForm() {
+        let self = this;
+
+        self.loginForm = this.fb.group(
+            {
+                username: [
+                    '',
+                    Validators.compose([
+                        Validators.required,
+                        Validators.minLength(5),
+                        Validators.maxLength(25),
+                    ]),
+                ],
+                password: ['', Validators.compose([Validators.required])],
+            },
+            { updateOn: 'blur' },
+        );
     }
-  }
+
+    onEvent(event: string) {
+        let self = this;
+
+        if (event === 'login') {
+            let result = self.auth
+                .signIn(self.loginForm.value)
+                .then(result => {
+                    console.log('authUser', result);
+                })
+                .catch(err => {
+                    console.error('signIn', err);
+                });
+        }
+
+        if (event === 'signOut') {
+            console.log('signOut', event);
+            self.auth.signOut();
+        }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,21 @@ import { AppComponent } from './app.component';
 import { NgxAmplifyModule } from 'projects/ngx-amplify/src/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    AppRoutingModule,
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    FormsModule,
-    ReactiveFormsModule,
-    NgxAmplifyModule.forRoot({
-      region:'us-east-x',
-      userPoolId: 'us-east-1_kT3FBpRxx',
-      appId: '1r9vg3ob81jamk62mjepejd3dq',
-      idpUrl: `cognito-idp.us-east-x.amazonaws.com`,
-      identityPoolId: 'us-east-1:08f3112b-cc65-49e4-8063-81f16cccd1ax'
-    }),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        AppRoutingModule,
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        FormsModule,
+        ReactiveFormsModule,
+        NgxAmplifyModule.forRoot({
+            region: 'us-east-x',
+            userPoolId: 'us-east-1_kT3FBpRxx',
+            appId: '1r9vg3ob81jamk62mjepejd3dq',
+            idpUrl: `cognito-idp.us-east-x.amazonaws.com`,
+            identityPoolId: 'us-east-1:08f3112b-cc65-49e4-8063-81f16cccd1ax',
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
